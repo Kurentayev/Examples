bf4b98d4f53b2a34ba606ebe9d2188388a56b1df
Kurentayev <280200@list.ru>
2023-04-06 21:17:52 +0600

    Example 005 Condition If Else

---
+++ Example005_ConditionIfElse/Program.cs
@@ -0,0 +1,13 @@
+﻿
+Console.WriteLine("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "маша")
+{
+    Console.WriteLine("УРА, это же маша");
+}
+else 
+{
+    Console.WriteLine("ПРИВЕТ, ");
+    Console.WriteLine(username);
+}
